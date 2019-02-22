2dc049f1398093a32f68a49eee2d9780034874aa
helmkamp <andrew.helmkamp@valid.com>
2019-02-22 10:08:50 -0500

    init

---
+++ HtmlToPdf/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HtmlToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string html = "<p>I can't abide these Jawas. Disgusting creatures. Go on, go on. I can't understand how we got by those troopers.</p>";
+
+            var renderer = new IronPdf.HtmlToPdf();
+            var pdf = renderer.RenderHtmlAsPdf(html);
+            pdf.SaveAs("htmlString.pdf");
+            Console.WriteLine(pdf.BinaryData);
+        }
+    }
+}
